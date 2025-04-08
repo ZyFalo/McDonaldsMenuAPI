3d59d73527b2cb40a0d32cbc8d06b9a2c60ecaaa
ZyFalo <williamandres1603@gmail.com>
2025-04-07 20:52:39 -0500

    Fix MongoDB URI by correcting password format

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os
 
 # URI de conexión a MongoDB Atlas (reemplaza con tu URI)
-MONGO_URI = "mongodb+srv://williampena:1006506574@cluster0.zgcor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://williampena:1006506574ww@cluster0.zgcor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Conexión al cliente MongoDB
 client = MongoClient(MONGO_URI)
