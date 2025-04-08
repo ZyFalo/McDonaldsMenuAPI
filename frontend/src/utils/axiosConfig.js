9e74b107d776d3feda18e08a8daf5485feac1a84
ZyFalo <williamandres1603@gmail.com>
2025-04-08 00:09:07 -0500

    Fix API_URL in axiosConfig.js to include the full URL scheme

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "mcdonaldsmenuapi-production.up.railway.app";
+const API_URL = "https://mcdonaldsmenuapi-production.up.railway.app";
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
